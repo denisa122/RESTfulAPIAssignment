b0ad84c4fb16fdb44592c6cc30806da0696a4fd1
denisa122 <neagudenisa12@yahoo.com>
2024-02-21 21:32:03 +0100

    movie routes - READ

--- routes/movie.js
+++ routes/movie.js
@@ -5,6 +5,7 @@ const movie = require("../models/movie");
 // CRUD
 
 // Create
+
 // /api/movies/
 router.post("/", (request, response) => {
 
@@ -14,14 +15,38 @@ router.post("/", (request, response) => {
     // Insert data in the db 
     movie.insertMany(data)
     .then(data => { response.send(data); })
-    .catch(error => {response.status(500).send( { message: error.message }); })
+    .catch(error => { response.status(500).send( { message: error.message }); })
 })
 
+// Read
 
-
-// Read -- get
 // All movies
+// /api/movies/
+router.get("/", (request, response) => {
+
+    movie.find()
+    .then(data => { response.send(data); })
+    .catch(error => { response.status(500).send( {message: error.message}); })
+})
+
+// All movies with specific director
+// /api/movies/director
+router.get("/director", (request, response) => {
+
+    movie.find({ director: "Nick Cassavetes" } )
+    .then(data => { response.send(data); })
+    .catch(error => { response.status(500).send( {message: error.message}); })
+})
+
 // Specific movie
+// /api/movies/:id
+router.get("/:id", (request, response) => {
+
+    movie.findById(request.params.id)
+    .then(data => { response.send(data); })
+    .catch(error => { response.status(500).send( {message: error.message}); })
+})
+
 
 // Update -- put
 
